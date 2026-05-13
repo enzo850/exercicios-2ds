ad0299cddbf7b25afd8fc22b11aba70ccbb38220
Enzofelixx <francisco.felix144@aluno.ce.gov.br>
2026-05-13 19:21:14 +0000

    quase

---
+++ 2-bimestre/programacao-web/aula03-criando-um-projeto-com-vite/React/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  const name = "Enzo"
+  return(
+    <div>
+      <h1>Meu Primeiro Projeto React com Vite</h1> 
+      <h1>Meu nome é {name}</h1>
+    </div>
+  )
+}
