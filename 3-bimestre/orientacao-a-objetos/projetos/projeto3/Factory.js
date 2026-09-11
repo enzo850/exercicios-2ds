def62354a04dcbf37135d7ddd42b12981c407439
Enzofelixx <francisco.felix144@aluno.ce.gov.br>
2026-09-11 18:50:21 +0000

    quase

--- 3-bimestre/orientacao-a-objetos/projetos/projeto3/Factory.js
+++ 3-bimestre/orientacao-a-objetos/projetos/projeto3/Factory.js
@@ -1,7 +1,34 @@
 const Notificacao = require("./Notificacao")
 // CRIE SUA SOLUÇÃO ABAIXO ================
 
+const {
+    Email,
+    SMS,
+    App
+} = require("./notificacao")
 
+class NotificacaoFactory {
+
+    static criar(tipo, mensagem) {
+
+        switch (tipo) {
+
+            case "email":
+                return new Email(mensagem)
+
+            case "sms":
+                return new SMS(mensagem)
+
+            case "app":
+                return new App(mensagem)
+
+            default:
+                throw new Error("Tipo de notificação inválido")
+        }
+    }
+}
+
+module.exports = NotificacaoFactory
 
 
 
