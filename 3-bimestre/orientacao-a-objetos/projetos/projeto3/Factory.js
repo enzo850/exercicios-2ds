82695c138484ae593737252681f3b4709cc8a52b
Enzofelixx <francisco.felix144@aluno.ce.gov.br>
2026-09-11 19:12:42 +0000

    ahh majó

--- 3-bimestre/orientacao-a-objetos/projetos/projeto3/Factory.js
+++ 3-bimestre/orientacao-a-objetos/projetos/projeto3/Factory.js
@@ -1,35 +1,36 @@
 const Notificacao = require("./Notificacao")
 // CRIE SUA SOLUÇÃO ABAIXO ================
+const Notificacao = require("./Notificacao");
 
-const {
-    Email,
-    SMS,
-    App
-} = require("./notificacao")
+const Email = Notificacao.Email;
+const SMS = Notificacao.SMS;
+const App = Notificacao.App;
 
 class NotificacaoFactory {
 
     static criar(tipo, mensagem) {
 
         switch (tipo) {
-
             case "email":
-                return new Email(mensagem)
+                return new Email(mensagem);
 
             case "sms":
-                return new SMS(mensagem)
+                return new SMS(mensagem);
 
             case "app":
-                return new App(mensagem)
+                return new App(mensagem);
 
             default:
-                throw new Error("Tipo de notificação inválido")
+                throw new Error("Tipo de notificação inválido");
         }
     }
 }
 
-module.exports = NotificacaoFactory
-
+module.exports = NotificacaoFactory;
+module.exports = Notificacao;
+module.exports.Email = Email;
+module.exports.SMS = SMS;
+module.exports.App = App;
 
 
 
--- 3-bimestre/orientacao-a-objetos/projetos/projeto3/Notificacao.js
+++ 3-bimestre/orientacao-a-objetos/projetos/projeto3/Notificacao.js
@@ -1,44 +1,36 @@
 // CRIE SUA SOLUÇÃO ABAIXO ================
-
 class Notificacao {
     constructor(mensagem) {
-        this.mensagem = mensagem
+        this.mensagem = mensagem;
     }
 
     enviar() {
-        throw new Error("Método enviar deve ser implementado")
+        return this.mensagem;
     }
 }
 
 class Email extends Notificacao {
     enviar() {
-        return `E-mail enviado: ${this.mensagem}`
+        return `Email enviado: ${this.mensagem}`;
     }
 }
 
 class SMS extends Notificacao {
     enviar() {
-        return `SMS enviado: ${this.mensagem}`
+        return `SMS enviado: ${this.mensagem}`;
     }
 }
 
 class App extends Notificacao {
     enviar() {
-        return `Notificação no aplicativo: ${this.mensagem}`
+        return `Notificação no App: ${this.mensagem}`;
     }
 }
 
-module.exports = {
-    Notificacao,
-    Email,
-    SMS,
-    App
-}
-
-
-
-
-
+module.exports = Notificacao;
+module.exports.Email = Email;
+module.exports.SMS = SMS;
+module.exports.App = App;
 // === FIM DO CÓDIGO =======================
 // === NÃO FAZER NADA ABAIXO DESSA LINHA ===
 module.exports = Notificacao
